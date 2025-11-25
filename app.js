5ab9b1943d0b91197b031c04440af43d8f99153a
Sujatha <116954528+21CSE49@users.noreply.github.com>
2025-11-25 23:00:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello! Your Node App is Running Successfully!");
+    res.end("Hello, your Node App is running inside Docker & Kubernetes!");
 });
 
 server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server running on port 3000");
 });
